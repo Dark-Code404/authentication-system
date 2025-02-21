360a12c7ee25aa49a77871c5dac81d918ca1d30a
Dark-Code404 <luiyunish@gmail.com>
2025-02-21 22:42:56 +0545

    fixed settings

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'auths.CusUser'
 
